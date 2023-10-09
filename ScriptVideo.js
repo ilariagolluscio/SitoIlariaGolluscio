9a835295ada212d5ae4ad84e4a1196c560292bd2
ilariagolluscio <ilaria.golluscio@studio.unibo.it>
2023-10-10 01:18:44 +0200

    Correzione video e caricamento post

--- ScriptVideo.js
+++ ScriptVideo.js
@@ -7,22 +7,19 @@
 
       // Definisci gli elementi della lista
       const itemsArray = [
-          '<p id="Live" data-video-src="video/Ludovico Einaudi - _Elegy for the Arctic_ - Official Live (Greenpeace).mp4">Live Arctic</p><img id="Live" src="img/vinile.png" class="cd" >',
-          '<p id="Div" data-video-src="video/Ludovico Einaudi - Divenire (Live from The Royal Albert Hall London _ 2010).mp4">Divenire</p> <img id="Div" src="img/vinile.png" class="cd" >',
-          '<p id="Exp" data-video-src="video/Ludovico Einaudi - Experience (Official Visualizer).mp4">Experience</p> <img id="Exp" src="img/vinile.png" class="cd" >',
-          '<p id="Onde" data-video-src="video/Ludovico Einaudi - Le Onde (Live at the Old Vic Tunnels _ 2011).mp4">Le Onde</p> <img id="Onde" src="img/vinile.png" class="cd">',
-          '<p id="Nuv" data-video-src="video/Ludovico Einaudi - Nuvole Bianche (Official Visualiser).mp4">Nuvole Bianche</p> <img id="Nuv" src="img/vinile.png" class="cd">',
-          '<p id="Mat" data-video-src="video/Ludovico Einaudi – Una Mattina (Live A Fip 2015).mp4">Una Mattina</p> <img id="Mat" src="img/vinile.png" class="cd" >',
-          '<p id="Walk" data-video-src="video/Ludovico Einaudi - Walk (Official Music Video).mp4">Walk</p> <img id="Walk" src="img/vinile.png" class="cd">'
+          '<p id="Live" data-video-src="video/Ludovico Einaudi - _Elegy for the Arctic_ - Official Live (Greenpeace).mp4">Live Arctic</p><img id="Live" src="img/vinile.png" class="cd" data-video-src="video/Ludovico Einaudi - _Elegy for the Arctic_ - Official Live (Greenpeace).mp4">',
+          '<p id="Div" data-video-src="video/Ludovico Einaudi - Divenire (Live from The Royal Albert Hall London _ 2010).mp4">Divenire</p> <img id="Div" src="img/vinile.png" class="cd" data-video-src="video/Ludovico Einaudi - Divenire (Live from The Royal Albert Hall London _ 2010).mp4">',
+          '<p id="Exp" data-video-src="video/Ludovico Einaudi - Experience (Official Visualizer).mp4">Experience</p> <img id="Exp" src="img/vinile.png" class="cd" data-video-src="video/Ludovico Einaudi - Experience (Official Visualizer).mp4">',
+          '<p id="Onde" data-video-src="video/Ludovico Einaudi - Le Onde (Live at the Old Vic Tunnels _ 2011).mp4">Le Onde</p> <img id="Onde" src="img/vinile.png" class="cd"  data-video-src="video/Ludovico Einaudi - Le Onde (Live at the Old Vic Tunnels _ 2011).mp4">',
+          '<p id="Nuv" data-video-src="video/Ludovico Einaudi - Nuvole Bianche (Official Visualiser).mp4">Nuvole Bianche</p> <img id="Nuv" src="img/vinile.png" class="cd" data-video-src="video/Ludovico Einaudi - Nuvole Bianche (Official Visualiser).mp4">',
+          '<p id="Mat" data-video-src="video/Ludovico Einaudi – Una Mattina (Live A Fip 2015).mp4">Una Mattina</p> <img id="Mat" src="img/vinile.png" class="cd" data-video-src="video/Ludovico Einaudi – Una Mattina (Live A Fip 2015).mp4">',
+          '<p id="Walk" data-video-src="video/Ludovico Einaudi – Walk (Live A Fip 2015).mp4">Walk</p> <img id="Walk" src="img/vinile.png" class="cd"  data-video-src="video/Ludovico Einaudi – Walk (Live A Fip 2015).mp4">'
       ];
 
       // Aggiungi eventi di clic sugli elementi <li>, <img> e <p>
       circularList.addEventListener('click', function (evento) {
         let clickedIndex;
-        if (evento.target.tagName === 'LI') {
-          clickedIndex = Array.from(circularList.children).indexOf(evento.target);
-          
-        } else if (evento.target.tagName === 'IMG' || evento.target.tagName === 'P') {
+        if (evento.target.tagName === 'IMG') {
           clickedIndex = Array.from(circularList.children).indexOf(evento.target.parentElement);
         }
 
@@ -32,9 +29,13 @@
             // Non fare nulla
           } else if (clickedIndex < 3) {
             pos = 3 - clickedIndex;
+            let videoSrc = itemsArray[clickedIndex].match(/data-video-src="(.*?)"/)[1];
+            playVideo(videoSrc);
             spostaElementi(itemsArray, pos);
           } else {
             pos = itemsArray.length - (clickedIndex - 3);
+            let videoSrc = itemsArray[clickedIndex].match(/data-video-src="(.*?)"/)[1];
+            playVideo(videoSrc);
             spostaElementi(itemsArray, pos);
           }
 
@@ -46,11 +47,6 @@
 
           // Aggiungi la classe 'ingrandito' all'elemento cliccato
           circularList.children[3].classList.add('ingrandito');
-              // Ottenere il percorso del video dal data-video-src attributo
-          const videoSrc = itemsArray[clickedIndex].match(/data-video-src="(.*?)"/)[1];
-
-          // Chiamare la funzione per riprodurre il video
-          playVideo(videoSrc);
 
         }
       });
